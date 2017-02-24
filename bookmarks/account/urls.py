f8aecb35c2774e6399ab8c8ad9c29f79ad3f6a28
casol <casoltys@gmail.com>
2017-02-24 23:36:04 +0100

    Creating registration directory

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -3,5 +3,18 @@ from . import views
 
 urlpatterns = [
     # post views
-    url(r'^login/$', views.user_login, name='login'),
+    # url(r'^login/$', views.user_login, name='login'),
+
+    # Django authentication framework login / logout urls
+    url(r'^login/$',
+        'django.contrib.auth.views.login',
+        name='login'),
+
+    url(r'^logout/$',
+        'django.contrib.auth.views.logout',
+        name='logout'),
+
+    url(r'^logout-then-login/$',
+        'django.contrib.auth.views.logout_then_login',
+        name='logout_then_login'),
 ]
