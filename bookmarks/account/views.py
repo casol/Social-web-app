d8d95434429d28ceddce8e3702c1e772d541a87f
casol <casoltys@gmail.com>
2017-03-08 23:06:17 +0100

    Messages framwork for profile edit view

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -1,11 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login
-from .forms import LoginForm
 from django.contrib.auth.decorators import login_required
 from .forms import LoginForm, UserRegistrationForm
 from .forms import UserEditForm, ProfileEditForm
 from .models import Profile
+from django.contrib import messages
 
 
 @login_required
@@ -73,6 +73,10 @@ def edit(request):
         if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
             profile_form.save()
+            messages.success(request, 'Profile updated '\
+                                      'successfully')
+        else:
+            messages.error(request, 'Error updating your profile')
     else:
         user_form = UserEditForm(instance=request.user)
         profile_form = ProfileEditForm(instance=request.user.profile)
